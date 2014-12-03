aa554af737cda14c66fc3dbc22b06839cba7a0b2
MeePwn <maybewaityou@gmail.com>
2014-12-03 17:13:23 +0800

    test

--- TestPolling/src/com/llbt/testpolling/PollingUtils.java
+++ TestPolling/src/com/llbt/testpolling/PollingUtils.java
@@ -65,7 +65,6 @@ public class PollingUtils {
 	 */
 	public static void stopPollingService(Context context, Class<?> cls,
 			String action) {
-
 		AlarmManager manager = (AlarmManager) context
 				.getSystemService(Context.ALARM_SERVICE);
 		Intent intent = new Intent(context, cls);
